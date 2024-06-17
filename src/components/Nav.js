99149ce9b608b6fdc5ff5b761a576289f6b5bf85
Seb_105 <28schapfel1@gmail.com>
2024-06-17 15:46:25 -0400

    #2

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,24 +1,29 @@
-
-import React from 'react'; // ES6 js
-import {Link} from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Nav() {
-    return(
-    <sebastian class="Sebastain">
-        <nav class="navbar navbar-expand-lg navbar-dark bg-dark top">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMainMenu" aria-controls="navMainMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div id="navMainMenu" class="navbar-collapse collapse">
-                <div class="navbar-nav ml-auto navBarItems" >
-                    <Link to='/' className="nav-item nav-link"></Link>
-                    <Link to='/' className="nav-item nav-link">Home</Link>
-                    <Link to='/games' className="nav-item nav-link">games</Link>
-                </div>
-            </div>
-        </nav>
-    </sebastian >
-    );
+  return (
+    <div className="Sebastian">
+     
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark top" id='navbar1'>
+      <Link to="https://sebastian-105.org" className="nav-item nav-link" idName="Sebastian">
+          <img src="https://sebastian-105.org/105-noborder.png" style={{ borderRadius: '50%', width: '50px', height: '50px' }} alt="105" />
+      </Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navMainMenu" aria-controls="navMainMenu" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        
+        <div id="navMainMenu" className="navbar-collapse collapse">
+       
+          <div className="navbar-nav ml-auto navBarItems">      
+            <Link to="/" className="nav-item nav-link"></Link>
+            <Link to="/" className="nav-item nav-link">Home</Link>
+            <Link to="/games" className="nav-item nav-link">games</Link>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
 }
 
 export default Nav;
