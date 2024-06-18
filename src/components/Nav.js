d93ecb9c879ddf16e0c1863d6610a0de9c3814d2
Seb_105 <28schapfel1@gmail.com>
2024-06-18 17:48:23 -0400

    #4

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,17 +4,17 @@ import {Link} from 'react-router-dom';
 function Nav() {
 	return (
 		<div className="mainDiv">
-			<nav className="navbar navbar-expand-lg navbar-dark bg-dark top" id="navbar1">
-				<div id="navMainMenu" className="navbar-collapse collapse">
-					<Link to="https://sebastian-105.org" className="nav-item nav-link" idName="icon105" aria-expanded="false">
+			<nav className="navbar" id="navbar1">
+				<div className="navBarItems">
+					{/* <Link to="https://sebastian-105.org" className="nav-item nav-link" idName="icon105" aria-expanded="false">
 						<img src="https://sebastian-105.org/105-noborder.png" style={{borderRadius: '50%', width: '50px', height: '50px'}} alt="105" />
+					</Link> */}
+					<Link to="/" className="navBarLinks">
+						<i class="fa-solid fa-house"></i> Home
 					</Link>
-					<Link to="/" className="nav-item nav-link"></Link>
-					<Link to="/" className="nav-item nav-link">
-						Home
-					</Link>
-					<Link to="/games" className="nav-item nav-link">
-						games
+					<span className='divider'></span>
+					<Link to="/games" className="navBarLinks">
+						<i class="fa-solid fa-gamepad"></i> Games
 					</Link>
 					<div className="navbar-nav ml-auto navBarItems"></div>
 				</div>
